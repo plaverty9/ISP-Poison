691c2dde390ca1aeba1e9d027d85ca9e9d65b7bb
plaverty9 <patricklaverty+github@gmail.com>
2017-03-29 23:40:33 -0400

    Create poison.py

---
+++ poison.py
@@ -0,0 +1,22 @@
+import urllib
+import urllib2
+import random
+import time
+
+url=urllib2
+
+# uncomment and add a wordlist
+#wordlist =""
+
+f = open(wordlist,'r')
+tld = ['.com','.net','.org','.name','.us','.biz','.edu']
+for line in f:
+        line = line.rstrip()
+        ext = random.choice(tld)
+        site = line + ext
+        print("Getting "+site)
+        sleeptime = random.randint(2,3000)
+        time.sleep(sleeptime)
+        try: response = urllib2.urlopen('http://'+site)
+        except (urllib2.HTTPError, urllib2.URLError):
+                continue
