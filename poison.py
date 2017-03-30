10cbcb8508181a93b67a03b76e09d57990943983
plaverty9 <patricklaverty+github@gmail.com>
2017-03-30 09:56:37 -0400

    Update poison.py

--- poison.py
+++ poison.py
@@ -1,18 +1,33 @@
+import urllib
 import urllib2
 import random
 import time
+import linecache
 
-# uncomment and add a wordlist
-#wordlist =""
+# Use your own wordlist here
+wordlist =""
 
+i=0
+#Count the number of lines in the wordlist
 f = open(wordlist,'r')
-tld = ['.com','.net','.org','.name','.us','.biz','.edu']
 for line in f:
+        i+=1
+
+#Choose a random line to read
+linenum = random.randint(1,i)
+line = linecache.getline(wordlist, linenum)
+
+tld = ['.com','.net','.org','.name','.us','.biz','.edu']
+
+# never stop, keep poisoning
+while True:
         line = line.rstrip()
         ext = random.choice(tld)
         site = line + ext
         print("Getting "+site)
+        # Sleeps for a random time between 2 seconds and 5 minutes for the next request
         sleeptime = random.randint(2,300)
+        print "Sleeping for "+str(sleeptime)+" seconds"
         time.sleep(sleeptime)
         try: response = urllib2.urlopen('http://'+site)
         except (urllib2.HTTPError, urllib2.URLError):
